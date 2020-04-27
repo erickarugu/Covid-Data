23f80729d4390cd8e6ad0b2bce58393b68108506
erickarugu32 <erickarugu32@gmail.com>
2020-04-27 23:49:23 +0300

    Added a doughnut chart and improved on design

--- main.js
+++ main.js
@@ -1,31 +1,78 @@
-req = new XMLHttpRequest();
+const today = document.getElementById('today');
+const cases = document.getElementById('cases');
+const deaths = document.getElementById('deaths');
+const recovered = document.getElementById('recovered');
+const loader = document.getElementById('loader');
+const content = document.getElementById('content');
+const pieCtx = document.getElementById('pie').getContext('2d');
 
-req.open('GET', 'https://coronavirus-19-api.herokuapp.com/countries');
-req.responseType = 'text';
-req.onload = function () {
-    let data = JSON.parse(req.responseText);
-    data = data[7];
-    console.log(data);
-    let [todayCases, totalCases, totalDeaths, totalRecovered] = [data.todayCases, data.cases, data.deaths, data.recovered];
-    let today = document.getElementById('today');
-    let cases = document.getElementById('cases');
-    let deaths = document.getElementById('deaths');
-    let recovered = document.getElementById('recovered');
 
-    today.innerHTML = todayCases.toLocaleString();
-    cases.innerHTML = totalCases.toLocaleString();
-    deaths.innerHTML = totalDeaths.toLocaleString();
-    recovered.innerHTML = totalRecovered.toLocaleString();
-}
-
-req.send();
-
-// set date
-let myVar = setInterval(getTime, 1000);
-
-function getTime() {  
+// set Local date and Time
+setInterval(function getTime() {
     let time = new Date();
     time = time.toLocaleString();
     document.getElementById('date').innerHTML = time;
-    console.log(time);
+}, 1000);
+
+getData();
+
+function getTime() {
+    var time = new Date();
+    time = time.toLocaleString();
+    document.getElementById('date').innerHTML = time;
 }
+
+function getData() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://coronavirus-19-api.herokuapp.com/countries');
+    xhr.responseType = 'text';
+
+    xhr.onload = function () {
+        let data = JSON.parse(xhr.responseText);
+        data = Array.from(data);
+        data = data.find((record) => record.country === "World");
+
+        let [todayCases, totalCases, totalDeaths, totalRecovered] = [data.todayCases, data.cases, data.deaths, data.recovered];
+
+        today.innerHTML = todayCases.toLocaleString();
+        cases.innerHTML = totalCases.toLocaleString();
+        deaths.innerHTML = totalDeaths.toLocaleString();
+        recovered.innerHTML = totalRecovered.toLocaleString();
+
+        getChartsData(data);
+        content.classList.remove('hidden');
+        loader.classList.add('hidden');
+    }
+
+    xhr.send();
+}
+
+function getChartsData(data) {
+    dataLabels = Array.from(["Active", "Today", "Deaths", "Recovered"]);
+    chartData = Array.from([data.active, data.todayCases, data.deaths, data.recovered]);
+
+    // doughnut Chart
+    doughnutChart.data.labels = dataLabels;
+    doughnutChart.data.datasets[0].data.push(...chartData);
+    doughnutChart.update();
+}
+
+// Pie Chart
+var doughnutChart = new Chart(pieCtx, {
+    type: 'doughnut',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '# of Sales',
+            data: [],
+            backgroundColor: [
+                'orange',
+                'blue',
+                'red',
+                'green'
+            ],
+            borderWidth: 0
+        }]
+    }
+})
